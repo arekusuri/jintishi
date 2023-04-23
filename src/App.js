a6b0250723277e2b2ca50268b579a36e18482e4e
Alex Li <alex.li@chime.com>
2023-04-22 19:06:39 -0700

    add data

--- src/App.js
+++ src/App.js
@@ -1,22 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import TextFileDisplay from './TextFileDisplay';
+
 
 function App() {
+  const [showComponent, setShowComponent] = useState(false);
+
+  const handleClick = () => {
+    setShowComponent(!showComponent);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+		i am here
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
+        <a className="App-link"
+          href="data/categories.txt"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          see categoreis
         </a>
+        <a className="App-link"
+          href="data/characters.txt"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          see characters
+        </a>
+
+	<button onClick={handleClick}>
+        {showComponent ? 'Hide Component' : 'Show Component'}
+	</button>
+        {showComponent && <TextFileDisplay />}
       </header>
     </div>
   );
