bfdb8164d36afdc59d61127feeda0d10e95b39f6
Alex Li <alex.li@chime.com>
2023-05-01 23:49:17 -0700

    add textarea

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import React from 'react';
 import TextFileDisplay from './TextFileDisplay';
 import Home from './Home';
+import VerifyPoem from './VerifyPoem';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { NavLink } from 'react-router-dom';
 
@@ -9,6 +10,7 @@ const Menu = () => {
   return (
     <ul>
       <li> <NavLink to="/" activeClassName="active">Home</NavLink> </li>
+      <li> <NavLink to="/verify" activeClassName="active">查询</NavLink> </li>
       <li> <NavLink to="/categories" activeClassName="active">四声</NavLink> </li>
       <li> <NavLink to="/characters" activeClassName="active">平水韵表</NavLink> </li>
     </ul>
@@ -22,6 +24,7 @@ function App() {
         <Menu />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/verify" element={<VerifyPoem />} />
           <Route path="/categories" element={<TextFileDisplay filename="categories.txt"/>} />
           <Route path="/characters" element={<TextFileDisplay filename="characters.txt"/>} />
         </Routes> 
---
+++ src/VerifyPoem.js
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+
+function App() {
+  const [text, setText] = useState('');
+  
+  function handleSubmit() {
+    console.log(text);
+  }
+  
+  return (
+    <div>
+      <hr></hr>
+      <textarea value={text} onChange={(e) => setText(e.target.value)} 
+      style={{
+        width: '500px',
+        height: '400px',
+        border: '1px solid black'
+      }}
+      />
+      <button onClick={handleSubmit}>Submit</button>
+    </div>
+  );
+}
+
+export default App;
