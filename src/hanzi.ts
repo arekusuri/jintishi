3bf2a15aa088e7bd448b9e88dcaed26b1f85ea9c
Alex Li <alex.li@chime.com>
2023-05-06 15:39:19 -0700

    fix data

--- src/hanzi.ts
+++ src/hanzi.ts
@@ -1,10 +1,12 @@
 class Hanzi {
     public zi: string;
     public is_ping: boolean;
+    public sisheng: string;
     public category: string;
-    constructor(zi: string, is_ping: boolean, category: string) {
+    constructor(zi: string, is_ping: boolean, sisheng: string, category: string) {
         this.zi = zi;
         this.is_ping = is_ping;
+        this.sisheng = sisheng
         this.category = category;
     }
 }
--- src/parse-file.ts
+++ src/parse-file.ts
@@ -13,15 +13,29 @@ class Characters {
     parse() {
       const text = this.text;
       const dict: Record<string, Hanzi> = {};
-      const arr = text.split(/\r?\n/).filter(line => line.trim() !== "");
+      const clean_text = text.replace(/\[[^\[\]]*\]|,|。|、|,/g, "");
+      const arr = clean_text.split(/\r?\n/).filter(line => line.trim() !== "");
       for (let i = 0; i < arr.length; i += 2) {
         const info = arr[i]
         const list =  Array.from(arr[i + 1]);
         for (var c of list) {
-          dict[c] = new Hanzi(c, info.includes('平'), info)
+          dict[c] = new Hanzi(c, info.includes('平'), this.decide_sisheng(info), info)
         }
       }
       return dict;
+    }
+
+    decide_sisheng(category: string): string {
+      if (category.includes("上平") || category.includes("下平")) {
+        return "平";
+      } else if (category.includes("上")) {
+        return "上";
+      } else if (category.includes("去")) {
+        return "去";
+      } else {
+        return "入";
+      }
+
     }
 
     async load() {
