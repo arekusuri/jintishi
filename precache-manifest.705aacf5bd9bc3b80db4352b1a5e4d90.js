82cc4483207722463c878ccc43b0e3c32b292745
arekusuri <arekusuri@users.noreply.github.com>
2023-05-06 19:57:25 +0000

    deploy: e8c0fb2e2795fedccc8100eb026cb2b330bd9048

---
+++ precache-manifest.705aacf5bd9bc3b80db4352b1a5e4d90.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "9526e4a585c276f9dccb",
+    "url": "/pingshuiyun/static/css/main.832fcf78.chunk.css"
+  },
+  {
+    "revision": "9526e4a585c276f9dccb",
+    "url": "/pingshuiyun/static/js/main.9526e4a5.chunk.js"
+  },
+  {
+    "revision": "f881440d46e811047be5",
+    "url": "/pingshuiyun/static/js/1.f881440d.chunk.js"
+  },
+  {
+    "revision": "8c2b5a41f17134b20bed",
+    "url": "/pingshuiyun/static/css/2.f6d8de31.chunk.css"
+  },
+  {
+    "revision": "8c2b5a41f17134b20bed",
+    "url": "/pingshuiyun/static/js/2.8c2b5a41.chunk.js"
+  },
+  {
+    "revision": "95d53d4a652abbd84e57",
+    "url": "/pingshuiyun/static/js/runtime~main.95d53d4a.js"
+  },
+  {
+    "revision": "a4ca93355b8720d440d91897a2561324",
+    "url": "/pingshuiyun/index.html"
+  }
+];
